4a2eada031453a4b6a85db7df1c3b63b5e7b17a3
Phathdt <phathdt379@gmail.com>
2021-08-17 23:15:02 +0700

    add hookform util

--- src/App.js
+++ src/App.js
@@ -1,38 +1,71 @@
 import { useForm } from 'react-hook-form'
-import { Button, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { ErrorMessage } from '@hookform/error-message'
+import { DevTool } from '@hookform/devtools'
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6).max(10),
+})
 
 function App() {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
 
   function onSubmit(data) {
-    console.log(data)
+    console.log(JSON.stringify(data, null, 2))
   }
 
   return (
     <div className="container">
-      <Row className="d-flex justify-content-center mt-5">
-        <Col sm={4}>
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <FormGroup className="mb-2">
-              <Label className="mb-2">Email</Label>
-              <Input {...register('email')} placeholder="abc@mail.com" />
-            </FormGroup>
+      <DevTool control={control} placement="top-right" />
 
-            <FormGroup className="mb-2">
-              <Label className="mb-2">Password</Label>
-              <Input
-                {...register('password')}
-                type="password"
-                placeholder="password"
+      <div className="d-flex justify-content-center mt-5">
+        <div className="col-sm-4">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group mb-2">
+              <label>Email</label>
+              <input
+                name="email"
+                type="text"
+                {...register('email')}
+                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
               />
-            </FormGroup>
+              <div className="invalid-feedback">
+                <ErrorMessage errors={errors} name="email" />
+              </div>
+            </div>
 
-            <Button color="success" className="mt-2">
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
+            <div className="form-group mb-2">
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                {...register('password')}
+                className={`form-control ${
+                  errors.password ? 'is-invalid' : ''
+                }`}
+              />
+              <div className="invalid-feedback">
+                <ErrorMessage errors={errors} name="password" />
+              </div>
+            </div>
+
+            <div className="form-group d-flex justify-content-center mt-3">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
